e9e150e10c35cb1b2b09f52b8edde9f9e2d0709f
Zeeshan Abbas <zeeshanabbasrao@gmail.com>
2024-07-26 06:41:22 -0700

    we just write an indirect recurrsion code in js file

--- Recursion/indirectrecurrsion/script.js
+++ Recursion/indirectrecurrsion/script.js
@@ -1,1 +1,19 @@
-console.log("hi there");
+
+let money = 20;
+let total = 2000;
+
+function buy(x) {
+    if(x>0){
+        console.log(`i have RS ${x} and i can but ${total} apples`)
+buymore(x);
+
+    }
+    
+     
+    }
+    function buymore(x) {
+        total= total-100;
+        buy(x-2);
+    }
+
+buy(money);
